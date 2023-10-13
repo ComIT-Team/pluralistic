c8c87af929dda2d79695cd4d58f97803f5106dc4
Pavubrar <56096198+Pavubrar@users.noreply.github.com>
2023-10-12 22:03:02 -0600

    logo added

--- pluralistic/src/components/navbar/Navbar.jsx
+++ pluralistic/src/components/navbar/Navbar.jsx
@@ -9,24 +9,24 @@ import Products from '../Products'
 import { Link } from 'react-router-dom'
 import Home from "../Home"
 //import "./Navbar.css";
-//import logo from '../../assets/img20pl.svg';
+import logo from '../../assets/logo-transparent-svg.svg';
 //import { MenuData } from './MenuData';
 const Navbar = () => {
   return (
     <header className='container mx-auto px-4 py-6 flex items-center justify-between no-underline'>
-<Link to='/' element={Home}><h1 className='font-bold text-white no-underline text-xl'>Pluralistic</h1></Link>
-    <nav  >
-    {/* <div className='container mx-auto px-4 py-6 flex items-center justify-between'>
-        
-        <img src={logo} className='align-center mx-6 md:cursor-pointer h-24 w-16 rounded-full  ' alt="logo"></img>
-        </div> */}
-        
-        <ul className='flex items-center justify-center font-semibold'>
+{/* <Link to='/' element={Home}><h1 className='font-bold text-white no-underline text-xl'>Pluralistic</h1></Link> */}
+    <nav className="grid grid-cols-3 gap-4  place-items-start "  >
+    <div className='container mx-auto px-4 py-0 flex self-start items-start '>
+        <Link to='/' element={Home}>
+        <img src={logo} className='self-start mx-6 md:cursor-pointer flex-grow h-[8em] w-[10em] rounded-full  ' alt="logo"></img></Link>
+        </div>
+        <div className="self-start">
+        <ul className='  flex items-center justify-center font-semibold mx-auto px-4 py-[3em] whitespace-nowrap '>
        
 
             <li  className='relative px-3 py-2 group'>
             <Link to="/AboutUs" element={About}>
-                <button className='hover:opacity-50 curser-default'
+                <button className='hover:opacity-50 curser-default text-white'
                 >About-Us</button>
             
                 <div className='absolute top-0 -left-48 transition group-hover:translate-y-5 translate-y-0 opacity-0 invisible group-hover:visible group-hover:opacity-100 duration-500 ease-in-out group-hover:transform z-50 min-w-[560px] transform bg-white'>
@@ -39,7 +39,7 @@ const Navbar = () => {
                 </Link>
             </li>
             <li  className='relative group px-3 py-2'>
-                <button className='hover:opacity-50 curser-default'
+                <button className='hover:opacity-50 curser-default text-white'
                 >Seller LogIn</button>
                 <div className='absolute top-0 left-0 transition group-hover:translate-y-5 translate-y-0 opacity-0 invisible group-hover:opacity-100 group-hover:visible duration-500 ease-in-out group-hover:transform z-50 min-w-[560px] transform'>
                   <div  className='relative top-6 p-6 bg-white rounded-xl shadow-xl w-full'>
@@ -50,7 +50,7 @@ const Navbar = () => {
                
             </li>
             <li  className='relative group px-3 py-2'>
-             <Link to='/products' element={Products}><button className='hover:opacity-50 curser-default'
+             <Link to='/products' element={Products}><button className='hover:opacity-50 curser-default text-white'
                 >Products</button>  </Link> 
                 <div className='absolute top-0 left-0 transition group-hover:translate-y-5 translate-y-0 opacity-0 invisible group-hover:opacity-100 group-hover:visible duration-500 ease-in-out group-hover:transform z-50 min-w-[560px] transform'>
                   <div  className='relative top-6 p-6 bg-white rounded-xl shadow-xl w-full'>
@@ -62,14 +62,14 @@ const Navbar = () => {
             </li>
         
         </ul>
-        
+        </div>
         
         </nav>
         <nav>
           <ul>
             <li>
-<Link to='/login' element={Login} className='rounded-full px-3 py-2 font-semibold bg-white bg-opacity-10 flex items-center group'>
-  <span className='mr-2 text-white'>Sign in</span>
+<Link to='/login' element={Login} className='rounded-full px-3 py-2 mt-0 font-semibold bg-white bg-opacity-10 flex items-center group text-white'>
+  <span className='mr-2 text-white whitespace-nowrap '>Sign in</span>
   <svg className="stroke-current stroke-2 
   " width="10" height="10" fill="none" viewBox="0 0 10 10" aria-hidden="true">
   <g fillRule="evenodd">
