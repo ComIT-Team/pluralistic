b142d8519f63564c6253f5c1f2d25366fd8951fc
Vedavyas Sai Ananthasetty <vedavyasananthasetty@gmail.com>
2023-10-16 12:56:39 -0400

    Changed the products to more generic format. Added filtering functions.

--- pluralistic/src/components/Cards.jsx
+++ pluralistic/src/components/Cards.jsx
@@ -5,28 +5,108 @@ const Cards = () => {
     <div className="max-w-[1680px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
         <div className='relative'>
             <div className='rounded-xl absolute w-full h-full bg-black/50 text-white p-4'>
-                <h2 className='font-bold text-2xl'>Fruits & <span className='text-emerald-300'>Vegetables</span></h2>
-                <p>Experience the vibrant goodness of nature with our <span className='text-emerald-300'>fresh
-                </span>, <span className='text-emerald-300'>hand-picked</span> fruits and vegetables</p>
-                <button className='rounded-xl bg-emerald-400/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
+                <h2 className='font-bold text-2xl'>Groceries</h2>
+                {/* <p>Experience the vibrant goodness of nature with our <span className='text-rose-500'>fresh
+                </span>, <span className='text-rose-500'>hand-picked</span> fruits and vegetables</p> */}
+                <button className='rounded-xl bg-rose-500/60 text-white p-1 m-2 absolute bottom-4 '>Order Now</button>
             </div>
             <img src="https://images.pexels.com/photos/3025236/pexels-photo-3025236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
         </div>
         <div className='relative'>
             <div className='rounded-xl absolute w-full h-full bg-black/50 text-white p-4'>
-                <h2 className='font-bold text-2xl'>Eggs, Meat & <span className='text-emerald-300'>Fish</span></h2>
-                <p>From farm-fresh produce to <span className='text-emerald-300'>premium meats</span>, we've got your plate covered with the <span className='text-emerald-300'>finest selection</span></p>
-                <button className='rounded-xl bg-emerald-400/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
+                <h2 className='font-bold text-2xl'>Electronics</h2>
+                {/* <p>From farm-fresh produce to <span className='text-rose-500'>premium meats</span>, we've got your plate covered with the <span className='text-rose-500'>finest selection</span></p> */}
+                <button className='rounded-xl bg-rose-500/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
             </div>
-            <img src="https://images.pexels.com/photos/161496/eggs-happen-food-ecology-161496.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
+            <img src="https://images.pexels.com/photos/4011762/pexels-photo-4011762.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
         </div>
         <div className='relative'>
             <div className='rounded-xl absolute w-full h-full bg-black/50 text-white p-4'>
-                <h2 className='font-bold text-2xl'>Beverages & <span className='text-emerald-300'>Snacks</span></h2>
-                <p>From energizing beverages to delightful snacks, we've got your <span className='text-emerald-300'>cravings</span> covered</p>
-                <button className='rounded-xl bg-emerald-400/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
+                <h2 className='font-bold text-2xl'>Clothing</h2>
+                {/* <p>From farm-fresh produce to <span className='text-rose-500'>premium meats</span>, we've got your plate covered with the <span className='text-rose-500'>finest selection</span></p> */}
+                <button className='rounded-xl bg-rose-500/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
             </div>
-            <img src="https://images.pexels.com/photos/952593/pexels-photo-952593.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
+            <img src="https://images.pexels.com/photos/5864264/pexels-photo-5864264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
+        </div>
+        <div className='relative'>
+            <div className='rounded-xl absolute w-full h-full bg-black/50 text-white p-4'>
+                <h2 className='font-bold text-2xl'>Kitchen</h2>
+                {/* <p>From farm-fresh produce to <span className='text-rose-500'>premium meats</span>, we've got your plate covered with the <span className='text-rose-500'>finest selection</span></p> */}
+                <button className='rounded-xl bg-rose-500/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img src="https://images.pexels.com/photos/6996085/pexels-photo-6996085.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
+        </div>
+        <div className='relative'>
+            <div className='rounded-xl absolute w-full h-full bg-black/50 text-white p-4'>
+                <h2 className='font-bold text-2xl'>Toys</h2>
+                {/* <p>From farm-fresh produce to <span className='text-rose-500'>premium meats</span>, we've got your plate covered with the <span className='text-rose-500'>finest selection</span></p> */}
+                <button className='rounded-xl bg-rose-500/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img src="https://images.pexels.com/photos/168866/pexels-photo-168866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
+        </div>
+        <div className='relative'>
+            <div className='rounded-xl absolute w-full h-full bg-black/50 text-white p-4'>
+                <h2 className='font-bold text-2xl'>Home Decor</h2>
+                {/* <p>From farm-fresh produce to <span className='text-rose-500'>premium meats</span>, we've got your plate covered with the <span className='text-rose-500'>finest selection</span></p> */}
+                <button className='rounded-xl bg-rose-500/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img src="https://images.pexels.com/photos/667838/pexels-photo-667838.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
+        </div>
+        <div className='relative'>
+            <div className='rounded-xl absolute w-full h-full bg-black/50 text-white p-4'>
+                <h2 className='font-bold text-2xl'>Personal care</h2>
+                {/* <p>From farm-fresh produce to <span className='text-rose-500'>premium meats</span>, we've got your plate covered with the <span className='text-rose-500'>finest selection</span></p> */}
+                <button className='rounded-xl bg-rose-500/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img src="https://images.pexels.com/photos/3685530/pexels-photo-3685530.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
+        </div>
+        <div className='relative'>
+            <div className='rounded-xl absolute w-full h-full bg-black/50 text-white p-4'>
+                <h2 className='font-bold text-2xl'>Books</h2>
+                {/* <p>From farm-fresh produce to <span className='text-rose-500'>premium meats</span>, we've got your plate covered with the <span className='text-rose-500'>finest selection</span></p> */}
+                <button className='rounded-xl bg-rose-500/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img src="https://images.pexels.com/photos/2128249/pexels-photo-2128249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
+        </div>
+        <div className='relative'>
+            <div className='rounded-xl absolute w-full h-full bg-black/50 text-white p-4'>
+                <h2 className='font-bold text-2xl'>Music & Video Games</h2>
+                {/* <p>From farm-fresh produce to <span className='text-rose-500'>premium meats</span>, we've got your plate covered with the <span className='text-rose-500'>finest selection</span></p> */}
+                <button className='rounded-xl bg-rose-500/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img src="https://images.pexels.com/photos/2876659/pexels-photo-2876659.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
+        </div>
+        <div className='relative'>
+            <div className='rounded-xl absolute w-full h-full bg-black/50 text-white p-4'>
+                <h2 className='font-bold text-2xl'>Baby Care</h2>
+                {/* <p>From farm-fresh produce to <span className='text-rose-500'>premium meats</span>, we've got your plate covered with the <span className='text-rose-500'>finest selection</span></p> */}
+                <button className='rounded-xl bg-rose-500/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img src="https://images.pexels.com/photos/3564304/pexels-photo-3564304.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
+        </div>
+        <div className='relative'>
+            <div className='rounded-xl absolute w-full h-full bg-black/50 text-white p-4'>
+                <h2 className='font-bold text-2xl'>Outdoor Essentials</h2>
+                {/* <p>From farm-fresh produce to <span className='text-rose-500'>premium meats</span>, we've got your plate covered with the <span className='text-rose-500'>finest selection</span></p> */}
+                <button className='rounded-xl bg-rose-500/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img src="https://images.pexels.com/photos/4311142/pexels-photo-4311142.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
+        </div>
+        <div className='relative'>
+            <div className='rounded-xl absolute w-full h-full bg-black/50 text-white p-4'>
+                <h2 className='font-bold text-2xl'>Jewelry</h2>
+                {/* <p>From farm-fresh produce to <span className='text-rose-500'>premium meats</span>, we've got your plate covered with the <span className='text-rose-500'>finest selection</span></p> */}
+                <button className='rounded-xl bg-rose-500/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img src="https://images.pexels.com/photos/9428788/pexels-photo-9428788.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
+        </div>
+        <div className='relative'>
+            <div className='rounded-xl absolute w-full h-full bg-black/50 text-white p-4'>
+                <h2 className='font-bold text-2xl'>Pet Care</h2>
+                {/* <p>From farm-fresh produce to <span className='text-rose-500'>premium meats</span>, we've got your plate covered with the <span className='text-rose-500'>finest selection</span></p> */}
+                <button className='rounded-xl bg-rose-500/60 text-white p-1 m-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img src="https://images.pexels.com/photos/757455/pexels-photo-757455.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="card" className='rounded-xl max-h-[160px] md:max-h-[200px] w-full object-cover'/>
         </div>
     </div>
   )
